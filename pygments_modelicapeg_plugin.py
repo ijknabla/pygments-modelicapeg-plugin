754d6469212b4b4cea665e4fadfaca7975be0c91
ijknabla <ijknabla@gmail.com>
2023-01-31 00:17:48 +0900

    Implement lexer

--- pygments_modelicapeg_plugin.py
+++ pygments_modelicapeg_plugin.py
@@ -1,18 +1,47 @@
-__all__ = (
-    "ModelicaPEGLexer",
-)
+__all__ = ("ModelicaPEGLexer",)
 
-from pygments.lexer import RegexLexer
-from pygments.token import Text
+from pygments.lexer import RegexLexer, bygroups, include
+from pygments.token import Comment, Keyword, Name, Operator, Punctuation, String, Text
 
 
 class ModelicaPEGLexer(RegexLexer):
     name = "ModelicaPeg"
-    aliases = ("modelica-peg", "modelicapeg",)
-    filenames = ("*.modelicapeg")
+    aliases = (
+        "modelica-peg",
+        "modelicapeg",
+    )
+    filenames = "*.modelicapeg"
 
     tokens = {
         "root": [
-            (r".", Text),
+            include("common"),
+        ],
+        "[": [
+            (r"\]", Punctuation, "#pop"),
+            include("common"),
+        ],
+        "{": [
+            (r"\}", Punctuation, "#pop"),
+            include("common"),
+        ],
+        "(": [
+            (r"\)", Punctuation, "#pop"),
+            include("common"),
+        ],
+        "common": [
+            (r"/\*([^*]|\*[^/])*\*/", Comment.Multiline),
+            (r"//.*", Comment.Single),
+            (r'"([^"]|"(?="))*"', String),
+            (r"'([^']|'(?='))*'", String),
+            (r"r'([^'\\]|\\.)*'", String.Regex),
+            (r'r"([^"\\]|\\.)*"', String.Regex),
+            (r"[:=|]", Operator),
+            (r"(`)([a-z]+)(`)", bygroups(None, Keyword, None)),
+            (r"[A-Z]([\-0-9A-Z]*[0-9A-Z])?", Name),
+            (r"[a-z]([\-0-9a-z]*[0-9a-z])?", Name),
+            (r"\[", Punctuation, "["),
+            (r"\{", Punctuation, "{"),
+            (r"\(", Punctuation, "("),
+            (r"\s+", Text),
         ],
     }
